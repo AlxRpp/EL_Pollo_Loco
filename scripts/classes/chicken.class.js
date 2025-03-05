9acf86d5905f69704839d5c5da566894d6b1bffa
AlxRpp <alexanderr26@outlook.de>
2025-03-05 07:59:43 +0100

    animate chicken

--- scripts/classes/chicken.class.js
+++ scripts/classes/chicken.class.js
@@ -1,11 +1,31 @@
-class Chicken extends MoveableObject{
+class Chicken extends MoveableObject {
+    y = 345
+    height = 80
+    width = 50
+    images_walking = [
+        'assets/images/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'assets/images/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'assets/images/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
 
-    constructor(){
+    ];
+
+
+    constructor() {
         super().loadImage('assets/images/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.x = 200 + Math.random() * 500
-        this.y = 345
-        this.height = 80
-        this.width = 50
+        this.x = 200 + Math.random() * 500;
+        this.loadImages(this.images_walking);
+        this.animate();
+    }
 
+
+
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.images_walking.length;
+            let path = this.images_walking[i];
+            this.img = this.imageChache[path];
+            this.currentImage++;
+        }, 70)
     }
 }
