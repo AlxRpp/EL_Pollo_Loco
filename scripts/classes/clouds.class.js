8ccf6e7b268e7ea466eb4dc26bf7b125bf1d5777
AlxRpp <alexanderr26@outlook.de>
2025-03-05 06:28:15 +0100

    animate clouds

--- scripts/classes/clouds.class.js
+++ scripts/classes/clouds.class.js
@@ -1,18 +1,19 @@
-class Clouds extends MoveableObject{
+class Clouds extends MoveableObject {
     y = 20;
     height = 250;
     width = 500;
-   
-    constructor(path){
-        super().loadImage(path)
-        this.x =  Math.random() * 200
+
+    constructor() {
+        super().loadImage('./assets/images/5_background/layers/4_clouds/1.png')
+        this.x = Math.random() * 200
         this.animate();
     }
 
-    animate(){
-        setInterval(()=>{
-        this.moveLeft();
 
-        }, 60)
+    animate() {
+        setInterval(() => {
+            this.x -= this.speed;
+
+        }, 1000 / 60);
     }
 }
--- scripts/classes/movableObject.class.js
+++ scripts/classes/movableObject.class.js
@@ -1,87 +1,21 @@
-class MoveableObject extends DrawableObject {
+class MoveableObject{
+    img;
+    x = 120;
+    y = 250;
+    width = 50;
+    height = 150;
     speed = .15;
-    otherDirection = false;
-    speedY = 0;
-    acceleration = 2.5;
-    energy = 100
-    lastHit = 0;
-    offset = {
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0
-    };
-    enemyDead = false;
 
-
-    hit() {
-        this.energy -= 5;
-        if (this.energy < 0) {
-            this.energy = 0
-        } else {
-            this.lastHit = new Date().getTime();
-        }
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path
     }
 
-    isDead() {
-        return this.energy == 0;
+    moveRight(){
+        console.log("moving Right");
     }
 
-    isHurt() {
-        let timePassed = new Date().getTime() - this.lastHit;
-        timePassed = timePassed / 1000;
-        return timePassed < .45;
+    moveLeft(){
+        console.log("moving Left");
     }
-
-    applyGravitation() {
-        setInterval(() => {
-            if (this.isAboveGround() || this.speedY > 0) {
-                this.y -= this.speedY;
-                this.speedY -= this.acceleration;
-            }
-        }, 1000 / 30)
-    }
-
-    isAboveGround() {
-        if (this instanceof ThrowableObject) {
-            return true;
-        } else {
-            return this.y < 330;
-        }
-    }
-
-    moveRight() {
-        this.x += this.speed;
-    }
-
-    moveLeft() {
-        this.x -= this.speed;
-    }
-
-    jump() {
-        this.speedY = 22;
-    }
-
-    playAnimation(images) {
-        let i = this.currentImage % images.length;
-        let path = images[i];
-        this.img = this.imageChache[path]
-        this.currentImage++
-    }
-
-    mirroredImage(ctx) {
-        ctx.save();
-        ctx.translate(this.x + this.width, this.y);
-        ctx.scale(-1, 1);
-        ctx.drawImage(this.img, 0, 0, this.width, this.height);
-        ctx.restore();
-    }
-
-    isColliding(mo) {
-        return this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
-            this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
-            this.x + this.offset.left < mo.x + mo.width - mo.offset.right &&
-            this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom;
-    }
-
 }
