d54f719869bd1bb5c2649d8a9655b76bcaba3ec0
AlxRpp <alexanderr26@outlook.de>
2025-03-05 06:28:15 +0100

    animate clouds

--- scripts/classes/clouds.class.js
+++ scripts/classes/clouds.class.js
@@ -1,11 +1,19 @@
-class Clouds extends MoveableObject{
+class Clouds extends MoveableObject {
     y = 20;
     height = 250;
     width = 500;
-   
-    constructor(){
+
+    constructor() {
         super().loadImage('./assets/images/5_background/layers/4_clouds/1.png')
-        this.x =  Math.random() * 200
-      
+        this.x = Math.random() * 200
+        this.animate();
+    }
+
+
+    animate() {
+        setInterval(() => {
+            this.x -= this.speed;
+
+        }, 1000 / 60);
     }
 }
