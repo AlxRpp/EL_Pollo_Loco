135252a93cef37b993fb8d5fa9d6090576f00fa3
AlxRpp <alexanderr26@outlook.de>
2025-03-05 18:55:43 +0100

    added Level-1

--- scripts/classes/level.class.js
+++ scripts/classes/level.class.js
@@ -2,24 +2,10 @@ class Level {
     enemies;
     clouds;
     backgrounds;
-    throwableObjects;
-    coins;
-    bottles;
-    level_end_x = 3100;
-    offset = {
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0
-    };
 
-
-    constructor(enemies, clouds, coins, bottles, throwableObjects, backgrounds){
-        this.enemies = enemies;
-        this.clouds = clouds;
-        this.throwableObjects = throwableObjects;
-        this.backgrounds = backgrounds;
-        this.coins = coins;
-        this.bottles = bottles
+    constructor(enemies, clouds, backgrounds){
+       this.enemies = enemies;
+       this.clouds = clouds;
+       this.backgrounds = backgrounds;
     }
 }
