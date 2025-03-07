772b72919766c01c80d3a75dbae3ee5a3005a3a3
AlxRpp <alexanderr26@outlook.de>
2025-03-07 12:40:31 +0100

    added throwable objects -- bottle

--- scripts/Levels/level1.js
+++ scripts/Levels/level1.js
@@ -10,6 +10,9 @@ const level1 = new Level(
     // new Clouds('./assets/images/5_background/layers/4_clouds/2.png'),
   ],
   [
+   
+  ],
+  [
     new Background('./assets/images/5_background/layers/air.png', -719),
     new Background('./assets/images/5_background/layers/3_third_layer/2.png', -719),
     new Background('./assets/images/5_background/layers/2_second_layer/2.png', -719),
@@ -26,9 +29,9 @@ const level1 = new Level(
     new Background('./assets/images/5_background/layers/3_third_layer/1.png', 719 * 2),
     new Background('./assets/images/5_background/layers/2_second_layer/1.png', 719 * 2),
     new Background('./assets/images/5_background/layers/1_first_layer/1.png', 719 * 2),
-    new Background('./assets/images/5_background/layers/air.png', 719*3),
-    new Background('./assets/images/5_background/layers/3_third_layer/2.png', 719*3),
-    new Background('./assets/images/5_background/layers/2_second_layer/2.png', 719*3),
-    new Background('./assets/images/5_background/layers/1_first_layer/2.png', 719*3),
+    new Background('./assets/images/5_background/layers/air.png', 719 * 3),
+    new Background('./assets/images/5_background/layers/3_third_layer/2.png', 719 * 3),
+    new Background('./assets/images/5_background/layers/2_second_layer/2.png', 719 * 3),
+    new Background('./assets/images/5_background/layers/1_first_layer/2.png', 719 * 3),
   ]
 );
--- scripts/classes/world.class.js
+++ scripts/classes/world.class.js
@@ -9,13 +9,14 @@ class World {
     level = level1;
 
 
+
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
-        this.checkCollision();
+        this.run();
     }
 
     setWorld() {
@@ -35,6 +36,8 @@ class World {
 
         this.addItemToCanvas(this.character);
         this.addObjectToCanvas(this.level.enemies);
+        this.addObjectToCanvas(this.level.throwableObjects);
+
         this.ctx.translate(-this.camera_x, 0);
         requestAnimationFrame(() => {
             this.draw();
@@ -57,14 +60,27 @@ class World {
         }
     }
 
-    checkCollision() {
+    run() {
         setInterval(() => {
-            this.level.enemies.forEach((enemy) => {
-                if (this.character.isColliding(enemy)) {
-                    this.character.hit();
-                    this.statusbar.setPercentage(this.character.energy)
-                }              
-            })
+            this.checkCollision();
+            this.checkThrowObjects();
         }, 100)
     }
+
+    checkCollision() {
+        this.level.enemies.forEach((enemy) => {
+            if (this.character.isColliding(enemy)) {
+                this.character.hit();
+                this.statusbar.setPercentage(this.character.energy);
+            }
+        });
+    }
+
+    checkThrowObjects (){
+        if (this.keyboard.d ) {
+            let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100)
+            this.level.throwableObjects.push(bottle);
+        }
+    }
+
 }
